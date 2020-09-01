511d975cb64c3e7600bcd545f9e3e2ca42c96255
Edward Amor <edward.amor3@gmail.com>
2020-09-01 12:30:00 -0400

    feat: complete initial project scaffolding
    add compressed raw dataset
    update directory readme
    update environment.yml base spec
    update requirements.txt with base spec
    update project setup.py

--- data/raw/README.md
+++ data/raw/README.md
@@ -4,5 +4,5 @@ The original, immutable data dump.
 
 | Raw Dataset Name |  Link to the Full Raw Dataset | Full Dataset Size (MB) |
 | ---------------: | ----------------------------: | ---------------------: |
-|    Raw Dataset 1 | [link](link/to/full/dataset1) |                  2,000 |
-|    Raw Dataset 2 | [link](link/to/full/dataset2) |                    300 |
+|    data.csv.xz   |         [link](./data.csv.xz) |                    4.1 |
+
--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ import setuptools
 setuptools.setup(
     name="src",
     version="1.0.0",
-    url="",
-    author="",
-    license="",
-    author_email="",
+    url="https://www.github.com/Skellet0r/realty-forecast",
+    author="Edward Amor",
+    license="GPL-3.0",
+    author_email="edward.amor3@gmail.com",
     packages=["src"],
-    install_requires=[],
+    install_requires=[
+        "pandas",
+        "scipy",
+        "numpy",
+        "statsmodels",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "plotly",
+    ],
 )
 
